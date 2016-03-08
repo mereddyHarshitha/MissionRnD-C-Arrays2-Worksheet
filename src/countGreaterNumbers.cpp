497eea32020a047d078902594fcf44223b7ef276
mereddyHarshitha <mereddyHarshitha@missionrnd.com>
2016-03-09 00:58:21 +0530

    Submit Commit

--- src/countGreaterNumbers.cpp
+++ src/countGreaterNumbers.cpp
@@ -20,6 +20,86 @@ struct transaction {
 	char description[20];
 };
 
+int get_year(char *dob){
+
+	int year = 0, i = 6, value;
+
+	for (i = 6; dob[i] != '\0'; i++){
+
+		value = (int)dob[i] - 48;
+		year = year * 10 + value;
+
+	}
+	return year;
+}
+
+int get_month(char *dob){
+
+	int month;
+
+	month = ((int)dob[3] - 48) * 10 + (int)dob[4] - 48;
+
+	return month;
+
+}
+
+int get_date(char *dob){
+
+	int date;
+
+	date = ((int)dob[0] - 48) * 10 + (int)dob[1] - 48;
+
+	return date;
+
+}
+
+
+int dateCmp(char *dob1, char *dob2) {
+
+	int year1, year2, month1, month2, date1, date2;
+
+	year1 = get_year(dob1);
+	year2 = get_year(dob2);
+
+	month1 = get_month(dob1);
+	month2 = get_month(dob2);
+
+	date1 = get_date(dob1);
+	date2 = get_date(dob2);
+
+	if (year1 < year2)
+		return -1;
+	else if (year2 < year1)
+		return 1;
+	else if (year1 == year2){
+		if (month1 < month2)
+			return -1;
+		else if (month2 < month1)
+			return 1;
+		else if (month1 == month2){
+			if (date1 < date2)
+				return -1;
+			else if (date2 < date1)
+				return 1;
+			else if (date1 == date2)
+				return 0;
+
+		}
+
+	}
+}
+
 int countGreaterNumbers(struct transaction *Arr, int len, char *date) {
-	return -1;
+	
+	int i = 0;
+	int count = 0;
+
+	while (i <len) {
+		if ((dateCmp(Arr[i].date, date)) <= 0)
+			count = count;
+		else
+			count++;
+		i++;
+	}
+	return count;
 }
