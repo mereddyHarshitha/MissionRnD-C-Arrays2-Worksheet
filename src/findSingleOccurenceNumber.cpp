baea7fbdae629e1f3eedf58131dc8da6018c1d4e
mereddyHarshitha <mereddyHarshitha@missionrnd.com>
2016-03-09 20:00:33 +0530

    Submit Commit

--- src/findSingleOccurenceNumber.cpp
+++ src/findSingleOccurenceNumber.cpp
@@ -15,6 +15,62 @@ There are better ways of solving the problem than a brute-force solution which i
 complexity .
 */
 
+#include <stdio.h>
+
 int findSingleOccurenceNumber(int *A, int len) {
-	return -1;
+
+	if (A == NULL)
+		return -1;
+
+	int k = 0;
+	int i;
+	int temp[100];
+	for (i = 0; i<len; i++)
+	{
+		int j;
+		for (j = 0; j<i; j++)
+		if (A[i] == A[j])
+			break;
+
+		if (i == j){
+			temp[k] = A[i];			//unique Array
+			k++;
+		}
+	}
+
+	int j, count;
+	int c[100];
+
+	for (i = 0; i<k; i++){
+		j = i;
+		count = 0;
+		while (j != len){
+			if (temp[i] == A[j]){
+				count++;
+			}
+			j++;
+		}
+		c[i] = count;						//count Array
+	}
+
+	int y;
+
+	for (i = 0; i<k; i++){
+		if (c[i] == 1){
+			j = i;
+			while (j<k){
+				if (c[j + 1] == 1){
+					y = -1;
+					break;
+				}
+				else
+					y = temp[i];
+				j++;
+			}
+			break;
+		}
+		else
+			y = -1;
+	}
+	return y;
 }
